352fc7aa8c2c62bcdccf93a95f884a036507aa2e
Alexey <alexeyip68@gmail.com>
2023-08-17 17:33:13 +0300

    used method pop() in array

---
+++ JavaScript_Algorithms_and_Data_Structures/delete_last_element_method_pop()/js/main.js
@@ -0,0 +1,8 @@
+// Налаштування
+const myArray = [["John", 23], ["cat", 2]];
+
+// Змініть код лише під цим рядком
+const removedFromMyArray = myArray.pop();
+
+console.log(removedFromMyArray);
+console.log(myArray);
