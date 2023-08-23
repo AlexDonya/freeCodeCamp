99d37fbcfb3e5d30c754d81552a5f6b6c51b86e1
Alexey <alexeyip68@gmail.com>
2023-08-23 23:54:46 +0300

    Completed 1 task

---
+++ JavaScript_Algorithms_and_Data_Structures/rest_parameter/js/main.js
@@ -0,0 +1,11 @@
+const sum = (...args) => {
+    const arg = [...args];
+    let total = 0;
+    for (let i = 0; i < arg.length; i++) {
+        total += arg[i];
+    }
+    return total;
+    // return arg;
+}
+
+console.log(sum(0, 1, 2, 3));
